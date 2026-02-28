e591fbb4db6dd30fe99eba38cbcf0b28ce8c3066
ByteForge92 <anantsheoran412@gmail.com>
2026-03-01 03:47:16 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -46,7 +46,7 @@ def load_vector_store():
 @st.cache_resource
 def load_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash-latest",
+        model="gemini-1.5-flash",
         google_api_key=st.secrets["GOOGLE_API_KEY"]
     )
 
